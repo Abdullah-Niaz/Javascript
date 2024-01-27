2f60d6d195fd975a3723074da421f24186937de0
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-01-28 02:07:25 +0500

    let,var,const

---
+++ 01_Basic.js
@@ -0,0 +1,9 @@
+// let,var,const
+
+let name = "Omer Ali";
+const cnic = 362014636045433;
+let address = "Lahore";
+let php;
+
+console.log(name,cnic,address,php);
+console.table([name,cnic,address,php]);
