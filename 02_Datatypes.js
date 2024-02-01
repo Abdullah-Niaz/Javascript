6b2173edeec729352d967c0b3d2140c6580661f6
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-02-01 10:44:31 +0500

    Data Types

---
+++ 02_Datatypes.js
@@ -0,0 +1,22 @@
+"use strict " // treat all the code as a newer js version
+
+// alert(3 + 3 ); // we are using node js not browser
+
+let name = "Abdullah"
+let age = 20
+let isLoggedIn = true
+
+// Number => 2 to power 53
+// bigint
+//  strings => ""
+// bolean => true/false
+// null => standalone values
+// undefined => not assigned anything
+// symbol => unique
+
+// Objets
+
+console.log(typeof(name));
+
+console.log( typeof undefined);
+console.log( typeof null);
