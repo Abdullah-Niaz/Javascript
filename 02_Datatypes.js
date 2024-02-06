38d8f915df7af7e154b2354f9b50c2164053b1f0
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-02-07 00:31:52 +0500

    Data Types

--- 02_Datatypes.js
+++ 02_Datatypes.js
@@ -1,5 +1,23 @@
 "use strict " // treat all the code as a newer js version
 
+
+// JavaScript has 8 Datatypes
+    // 1. String
+    // 2. Number
+    // 3. Bigint
+    // 4. Boolean
+    // 5. Undefined
+    // 6. Null
+    // 7. Symbol
+    // 8. Object    
+
+// The Object Datatype
+    // The object data type can contain:
+
+        // 1. An object
+        // 2. An array
+        // 3. A date
+
 // alert(3 + 3 ); // we are using node js not browser
 
 let name = "Abdullah"
@@ -24,3 +42,26 @@ console.log( typeof null);
 let s = ['a','b'];
 s[2] = 'c';
 console.log(s);
+
+
+
+
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
