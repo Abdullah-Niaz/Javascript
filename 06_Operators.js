1bcc12fb18445bcf3dc7ebaa2c3ca5f1a3298264
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-02-06 23:29:08 +0500

    alomost covered all operators

--- 06_Operators.js
+++ 06_Operators.js
@@ -39,3 +39,60 @@ console.log("Post Decrement"+ z7);
 // Pree decrement
 let z8 = --x;
 console.log("Pree Decrement: "+z8);
+
+
+
+// data Types are not Checked here
+console.log(x == y);
+console.log(x != y);
+// These also check the data Types
+console.log(x === y);
+console.log(x !== y );
+
+console.log( x > y );
+console.log( x < y );
+console.log( x <= y );
+console.log( x >= y );
+
+
+let text1 = "A";
+let text2 = "B";
+let result1 = text1 < text2;
+console.log("Because A = 65 & B = 66 so 65 < 66: "+result1)
+
+let text11 = "20";
+let text22 = "5";
+let result2 = text11 < text22;
+console.log(result2);
+
+
+let text111 = "John";
+let text222 = "Doe";
+let text3 = text1 + " " + text2;
+console.log(text3);
+
+
+let txt1 = "What a very ";
+txt1 += "nice day";
+console.log(txt1);
+
+
+{
+    let x = 5 + 5;
+let y = "5" + 5;
+let z = "Hello" + 5;
+console.log(x);
+console.log(y);
+console.log(z);
+}
+
+
+{
+    // javascript Logical Operators
+    let a = 20;
+    let b = 40;
+    console.log(a.instanceof);
+    console.log( a < b && (a + 50) > b);
+    console.log( a < b || (a + 50) < b);
+    console.log();
+}
