c0aa4cba2c4a232cf3757de6e917a2dc1941dcc2
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-02-07 01:17:21 +0500

    For Loop

---
+++ 04_Loops/for_loop.js
@@ -0,0 +1,12 @@
+// for(inilization ; condition ; update){
+//     statement;
+// }
+for(let x = 0 ; x <= 10 ; x++){
+    if( x % 2 !== 0 ){
+        console.log("Odd: " + x );
+    }
+    else{
+        console.log("Even: " + x)
+    }
+}
+
