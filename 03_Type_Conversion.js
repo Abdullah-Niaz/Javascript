c375196504186a8ddf4eb207ea9e041c97c4bf7f
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-02-06 19:43:32 +0500

    Type Conversion

--- 03_Type_Conversion.js
+++ 03_Type_Conversion.js
@@ -3,3 +3,19 @@ console.log(num);
 
 let valint = Number(num);
 console.log(valint);
+
+
+let score = '20';
+console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+let isloggedin = 1;
+console.log(isloggedin);
+console.log(typeof isloggedin);
+
+let boleanconversion = Boolean(isloggedin);
+console.log(boleanconversion);
+console.log(typeof boleanconversion);
