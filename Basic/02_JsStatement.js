63b795e797bfc2e79c554911ef8f0973d8e05e2e
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-02-07 00:42:28 +0500

    Basic & Operators

---
+++ Basic/02_JsStatement.js
@@ -0,0 +1,22 @@
+let x, y, z;    // Statement 1
+x = 5;          // Statement 2
+y = 6;          // Statement 3
+z = x + y;      // Statement 4
+console.log(z);
+
+
+
+function greet(name,lastName){
+    console.log("Hello " + name + " " + lastName);
+}
+
+greet("Omer", "Ali");
+greet("Hamza" ,"Tariq");
+greet("Talha", "Azhar");
+greet("Faheem","Saleem");
+
+
+function square(number){
+    return number * number;
+}
+console.log(square(2))
