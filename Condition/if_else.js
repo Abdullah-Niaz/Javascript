e4564ceb9d44828b1732040246b9b0bcfdca74f2
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-02-07 01:02:29 +0500

    if else

---
+++ Condition/if_else.js
@@ -0,0 +1,23 @@
+let x = 20;
+
+if(x >= 33 && x < 60){
+    console.log("Passed");
+}
+else if(x < 33){
+    console.log("Failed 🫡🤣");
+}
+else {
+    console.log("Other");
+}
+
+
+let time = 16;
+let greeting;
+if (time < 10) {
+  greeting = "Good morning";
+} else if (time < 20) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
+console.log(greeting);
