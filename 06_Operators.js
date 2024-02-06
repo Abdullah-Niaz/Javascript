f0e78d38bb22ce0e62aea3346ccd54f8029e9226
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-02-06 23:16:03 +0500

    arithmentic Operators

---
+++ 06_Operators.js
@@ -0,0 +1,41 @@
+let x = 5;
+let y = 2;
+
+// Addition
+let z = x + y;
+console.log("Addition: " + z);
+
+// Subtraction
+let zz = x - y;
+console.log("Subtraction: " +zz);
+
+//  Multipilcation
+let z1 = x * y;
+console.log("Multiplication: "+z1);
+
+// Power
+let z2 = x ** y;
+console.log("Power: " + z2);
+
+// Division 
+let z3 = x / y;
+console.log("Division: "+z3);
+
+// MOdulus 
+let z4 = x % y;
+console.log("Modulus: "+z4);
+
+// Increament Operator
+// Post Increamnet
+let z5 = x++;
+console.log("Post Increment: "+z5);
+// Pree Increamnet
+let z6 = ++x;
+console.log("Pree Increment: "+z6);
+// Decrement Operator
+// Post Decrement
+let z7 = x--;
+console.log("Post Decrement"+ z7);
+// Pree decrement
+let z8 = --x;
+console.log("Pree Decrement: "+z8);
