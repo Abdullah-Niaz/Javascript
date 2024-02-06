f677216d2d2b1204ac54b3cc9f218683aeb3e3e5
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-02-07 01:19:33 +0500

    while Loop

---
+++ 04_Loops/while_loop.js
@@ -0,0 +1,16 @@
+let x = 0;
+// while(x <= 10){
+//     console.log(x);
+//     x++;
+// }
+
+
+while(x <= 10){
+   if(x % 2 !== 0 ){
+       console.log("Odd: " + x);
+   }
+   else{
+    console.log("Even: "+ x)
+   }
+    x++;
+}
