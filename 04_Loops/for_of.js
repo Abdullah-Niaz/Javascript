dd8f05ef0b65ae8be62b255f587f0219ca181e62
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-02-07 13:36:13 +0500

    for of loop

--- 04_Loops/for_of.js
+++ 04_Loops/for_of.js
@@ -2,3 +2,14 @@ const colors = ['red','green','blue'];
 for (const key of colors){ 
     console.log(key);    
 }
+
+// Function to compute the product of p1 and p2
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+// Function to compute the product of p1 and p2
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+console.log(myFunction(2,3));
