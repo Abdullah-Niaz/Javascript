586dfdae33c45e946f7a080408457ba2a94d6707
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-02-08 00:21:10 +0500

    dates intro in html

---
+++ 01_Basic/10_dates.js
@@ -0,0 +1,7 @@
+const d = new Date();
+console.log(d);
+console.log(d.toString());
+// you can override it according to your need like
+const dd = new Date('2023-01-04');
+console.log(dd);
+console.log(dd.toDateString());
