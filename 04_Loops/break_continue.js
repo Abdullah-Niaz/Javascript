8127bf1868522c6eadcc939637de3e32e50018fc
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-02-07 14:48:51 +0500

    break & continue

---
+++ 04_Loops/break_continue.js
@@ -0,0 +1,13 @@
+let i  =  0;
+
+while(i <= 10 ){
+    // if(i == 5){
+    //     break;
+    // }
+    if(i == 5){
+        i++;
+        continue;
+    }
+    console.log("Iteration: " + i);
+    i++;
+}
