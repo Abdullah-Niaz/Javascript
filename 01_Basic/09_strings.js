59d85ab0bd54dde992aa5fd7f1cd6ddf278ea8c5
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-02-07 22:56:22 +0500

    string interpolation

---
+++ 01_Basic/09_strings.js
@@ -0,0 +1,15 @@
+// String interpolcation
+//  modern way
+const name = "Abdullah";
+const repoCount = 50;
+
+console.log(`My name is ${name} and my repocount is ${repoCount}`)
+
+let myName  = new String("Abdullah");
+
+console.log(myName);
+console.log(myName.charAt(3));
+console.log(myName.indexOf("l"));
+
+const substring = myName.substring(0,);
+console.log(substring)
