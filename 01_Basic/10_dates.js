906bde7a5ecdcdc0ab0a0b61bc90bd91744c2f95
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-02-10 16:39:32 +0500

    data time module

--- 01_Basic/10_dates.js
+++ 01_Basic/10_dates.js
@@ -5,3 +5,17 @@ console.log(d.toString());
 const dd = new Date('2023-01-04');
 console.log(dd);
 console.log(dd.toDateString());
+
+
+
+//? There are 9 ways to create a new date object:
+
+    //! new Date()
+    //! new Date(date string)
+    //! new Date(year,month)
+    //! new Date(year,month,day)
+    //! new Date(year,month,day,hours)
+    //! new Date(year,month,day,hours,minutes)
+    //! new Date(year,month,day,hours,minutes,seconds)
+    //! new Date(year,month,day,hours,minutes,seconds,ms)
+    //! new Date(milliseconds)
