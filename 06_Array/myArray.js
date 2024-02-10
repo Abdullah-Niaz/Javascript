d4ed24446a3931e316e73998f7210cd32181a5b4
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-02-10 23:40:54 +0500

    Basic of Array

---
+++ 06_Array/myArray.js
@@ -0,0 +1,15 @@
+const arr = [1,2,3,4,5,6,7];
+console.log(arr);
+
+arr.push(8);
+arr.push(31);
+arr.pop();
+
+arr.unshift(12);
+console.log(arr);
+arr.shift();
+console.log(arr);
+
+console.log(arr.includes(9));
+console.log(arr.indexOf(9))
+console.log(arr);
