433687bc033add08f25168c9b787f5ed29817729
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-02-11 15:30:19 +0500

    Array Methods

--- 06_Array/myArray.js
+++ 06_Array/myArray.js
@@ -7,9 +7,25 @@ arr.pop();
 
 arr.unshift(12);
 console.log(arr);
-arr.shift();
+arr.shift(); 
 console.log(arr);
 
 console.log(arr.includes(9));
 console.log(arr.indexOf(9))
+
+
+// Returns the String
+const newarr = arr.join();
+
 console.log(arr);
+console.log(newarr);
+
+const newar = new Array(1,2,3,4,5);
+console.log(newar);
+
+const new1 = newar.slice(1,3);
+console.log(new1);
+
+const new2 = newar.splice(1,3);
+console.log(new2);
+console.log(newar);
