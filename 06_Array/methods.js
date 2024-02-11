9cc3d694c791b489335343e3f611a6f9b2be7c1a
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-02-11 15:46:39 +0500

    methods

---
+++ 06_Array/methods.js
@@ -0,0 +1,12 @@
+const myar1 = ["A","B"];
+const myar2 = ["C","D"];
+
+// const d1 =  myar1.push(myar2);
+// console.log(d1);
+
+// const d2 = myar1.concat(myar2);
+// console.log(d2);
+
+// spread operator
+const newsp = [...myar1,...myar2];
+console.log(newsp);
