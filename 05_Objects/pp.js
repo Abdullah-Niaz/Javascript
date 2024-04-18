532055daf2fe9400e3334e144605101c4d9328b1
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-04-18 23:03:11 +0500

    Practice Object instances

---
+++ 05_Objects/pp.js
@@ -0,0 +1,27 @@
+const mysym = Symbol("symbol");
+
+const arr ={
+    name: "Talha",
+    "Full Name":"Talha Azhar",
+    age : 20,
+    mysym : "STring",
+    [mysym] : "newMySymb",
+    address: "lahore",
+    isLoggedIn :true,
+}
+
+// console.log(arr["Full Name"]);
+// console.log(arr.name,arr.age,arr.address,arr.isLoggedIn)
+// console.table(arr)
+// this key will act as a string and not a symbol
+// mysym: "mykey1",
+console.log(arr.mysym);
+console.log(typeof arr.mysym);
+
+
+// if you want to use the symbol as key then use the subcript method
+// [mysym]: "mykey2",
+console.log(arr[mysym]);
+console.log(typeof arr[mysym]);
+
+Object.freeze(arr); // so that it  cant be modified further 
