07790a94a8a7e2100bde325e660b0ba205c55256
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-07-23 23:54:45 +0500

    scope

---
+++ 08_Scope/Scope.js
@@ -0,0 +1,14 @@
+// scope 
+    // 1. Local Scope 
+        // Where elements are accessed within the block 
+    // 2. Global Scope
+        // where variable are accessed by everyone of the programs
+
+if(true){
+    // let a = 20; // local scope 
+    // const c = 40; // local scope 
+    var b = 30; // global scope 
+}
+// console.log(a);
+console.log(b);
+// console.log(c);
