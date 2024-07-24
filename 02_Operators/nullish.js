63ccc3513d29bedbc5336c5bc141a6dfe62e907e
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-07-25 00:00:16 +0500

    nulish operator

--- 02_Operators/nullish.js
+++ 02_Operators/nullish.js
@@ -1,3 +1,9 @@
 // Nulish colesing operator 
+// particularly designed for null and undefined
+// sometimes you work with database, and return answer may have two value
+// it check them, either it is null/undefined or some value.
+// if value is there it will return that
+// else null/undefined will be returned
+
 const val1 = 5 ?? 10;
 console.log(val1);
