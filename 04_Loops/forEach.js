592c45031a7aac3e3646e95f5b43b8803fbb2869
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-07-26 23:04:57 +0500

    foreach, filter

---
+++ 04_Loops/forEach.js
@@ -0,0 +1,23 @@
+const arr = [1,2,3,4,5];
+// arr.forEach((env) => {
+//     console.log(env);
+// })
+// const a = arr.forEach((env) => {
+//     console.log(env);
+// })
+// console.log(a);
+
+// const newArr = []
+// arr.forEach((env) => {
+//     if( env > 2 ){
+//         newArr.push(env);
+//     };
+// });
+
+// console.log(newArr);
+
+
+// const a  = arr.filter( (env) => {
+//     return env > 2;
+// })
+// console.log(a);
