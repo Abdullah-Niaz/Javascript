bd328c40893061d767e5b5d5cf9bab76ad533eeb
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-07-31 00:46:59 +0500

    dom element creation

---
+++ 10_DOM/script.js
@@ -0,0 +1,18 @@
+const div = document.createElement("div");
+const h = document.createElement("h1");
+const h1 = document.createElement("h1");
+const h2 = document.createElement("h1");
+const h3 = document.createElement("h1");
+h.textContent = "Hello world 0 ";
+h1.textContent = "Hello world 1 ";
+h2.textContent = "Hello world 2 ";
+h3.textContent = "Hello world 3 ";
+div.appendChild(h);
+div.appendChild(h1);
+div.appendChild(h2);
+div.appendChild(h3);
+document.body.appendChild(div)
+console.log(div.childNodes[2])
+console.log(div.firstChild)
+console.log(div.lastChild)
+console.log(div.ATTRIBUTE_NODE)
