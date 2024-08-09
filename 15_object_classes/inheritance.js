4a333fe04ae767095e9c5b65a2acd15738ae8139
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-08-09 23:47:11 +0500

    inheritance

---
+++ 15_object_classes/inheritance.js
@@ -0,0 +1,17 @@
+class user{
+    constructor(username){
+        this.username = username;
+    }
+
+    logMe(){
+        console.log(`Username is: ${this.username}`);
+    }
+}
+
+class Teacher extends user{
+    constructor(username, email,password){
+        super(username);
+        this.email = email;
+        this.password = password;
+    }
+}
